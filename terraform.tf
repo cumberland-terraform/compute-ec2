06fcb61703c6be57217b069fd4cf7da5e199b28f
grant <grant.moore@maryland.gov>
2024-06-20 14:41:26 -0400

    add alias to provider

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,11 @@
 terraform {
-  required_version    = ">= 1.5.0"
+  required_version          = ">= 1.5.0"
 
   required_providers {
-    aws               = {
-      source          = "hashicorp/aws"
-      version         = ">= 4.8.0"
+    aws                     = {
+      source                = "hashicorp/aws"
+      version               = ">= 4.8.0"
+      configuration_aliases = [ aws.core ]
     }
   }
 }
