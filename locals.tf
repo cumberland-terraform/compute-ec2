7f420c3bc4c8f2e4de03d8eec671a07605b94c4b
grant <grant.moore@maryland.gov>
2024-06-24 16:07:22 -0400

    update tags

--- locals.tf
+++ locals.tf
@@ -1,67 +1,67 @@
 locals {
     # resource tags
-    tags                ={
-      Name              = join(
-                            "-", 
-                            [
-                                module.lookup_data.service_abbr,
-                                module.lookup_data.agency_oneletterkey,
-                                module.lookup_data.account_threeletterkey,
-                                module.lookup_data.program_abbr,
-                                module.lookup_data.region_twoletterkey,
-                                module.lookup_data.account_env_threeletterkey,
-                                var.ec2_config.suffix
-                            ]
-                        )
-      CreationDate      = formatdate("YYYY-MM-DD", timestamp())
-      Account           = module.lookup_data.account_threeletterkey
-      Environment       = module.lookup_data.account_env_fourletterkey
-      Agency            = module.lookup_data.agency_oneletterkey
-      Program           = module.lookup_data.program_key
-      Region            = module.lookup_data.region_twoletterkey
-      "PCA Code"        = var.platform.pca
-      AutoBackup        = var.ec2_config.auto_backup
-      Schedule          = var.ec2_config.schedule
-      PrimaryContact    = var.ec2_config.contact
-      NewBuild          = var.ec2_config.new_build
+    tags                        = {
+      Name                      = join(
+                                    "-", 
+                                    [
+                                        module.lookup_data.service_abbr,
+                                        module.lookup_data.agency_oneletterkey,
+                                        module.lookup_data.account_threeletterkey,
+                                        module.lookup_data.program_abbr,
+                                        module.lookup_data.region_twoletterkey,
+                                        module.lookup_data.account_env_threeletterkey,
+                                        var.ec2_config.suffix
+                                    ]
+                                )
+      CreationDate              = formatdate("YYYY-MM-DD", timestamp())
+      Account                   = module.lookup_data.account_threeletterkey
+      Environment               = module.lookup_data.account_env_fourletterkey
+      Agency                    = module.lookup_data.agency_oneletterkey
+      Program                   = module.lookup_data.program_key
+      Region                    = module.lookup_data.region_twoletterkey
+      "PCA Code"                = var.platform.pca
+      AutoBackup                = var.ec2_config.auto_backup
+      Schedule                  = var.ec2_config.schedule
+      PrimaryContact            = var.ec2_config.contact
+      NewBuild                  = var.ec2_config.new_build
     }
     # resource prefix
-    prefix              = lower(
-                            join(
-                                "-", 
-                                [
-                                    module.lookup_data.service_abbr,
-                                    module.lookup_data.agency_oneletterkey,
-                                    module.lookup_data.account_threeletterkey,
-                                    module.lookup_data.program_abbr
-                                ]
-                            )
-                        )
+    prefix                      = lower(
+                                    join(
+                                        "-", 
+                                        [
+                                            module.lookup_data.service_abbr,
+                                            module.lookup_data.agency_oneletterkey,
+                                            module.lookup_data.account_threeletterkey,
+                                            module.lookup_data.program_abbr
+                                        ]
+                                    )
+                                )
     # Platform Defaults
-    ec2_defaults        = {
-        ebs_optimized   = true
-        encrypted       = true
-        monitoring      = true
+    ec2_defaults                = {
+        ebs_optimized           = true
+        encrypted               = true
+        monitoring              = true
     }
     # ssh key configuration
-    ssh_key_algorithm   = "RSA"
-    ssh_key_bits        = 4096
+    ssh_key_algorithm           = "RSA"
+    ssh_key_bits                = 4096
 
-    conditions                      = {
-        provision_ssh_key           = var.ec2_config.ssh_key_name == null
-        provision_kms_key           = var.ec2_config.kms_key_id == null
+    conditions                  = {
+        provision_ssh_key       = var.ec2_config.ssh_key_name == null
+        provision_kms_key       = var.ec2_config.kms_key_id == null
     }
     
-    kms_key_id                      = local.conditions.provision_kms_key ? (
-                                        module.kms[0].key.id
-                                    ) : (
-                                        var.ec2_config.kms_key_id
-                                    )
-    ssh_key_name                    = local.conditions.provision_ssh_key ? (
-                                        aws_key_pair.ssh_key[0].key_name 
-                                    ) : ( 
-                                        var.ec2_config.ssh_key_name
-                                    )         
+    kms_key_id                  = local.conditions.provision_kms_key ? (
+                                    module.kms[0].key.id
+                                ) : (
+                                    var.ec2_config.kms_key_id
+                                )
+    ssh_key_name                = local.conditions.provision_ssh_key ? (
+                                    aws_key_pair.ssh_key[0].key_name 
+                                ) : ( 
+                                    var.ec2_config.ssh_key_name
+                                )         
     # These are extra filters that have to be added to the AMI data query to ensure the results
     #   returned are unique
     ami_filters                 = strcontains(var.ec2_config.operating_system, "RHEL") ? [
@@ -84,22 +84,22 @@ locals {
         }
     ]
 
-    userdata_path       = strcontains(var.ec2_config.operating_system, "RHEL") ? (
+    userdata_path               = strcontains(var.ec2_config.operating_system, "RHEL") ? (
         # RHEL ```user-data``` EXTENSION
         "${path.module}/user-data/rhel/user-data.sh" 
     ) : (
         # WINDOWS ```user-data``` EXTENSION
         "${path.module}/user-data/windows/user-data.ps1" 
     )
-    userdata_config     = strcontains(var.ec2_config.operating_system, "RHEL") ? {
+    userdata_config             = strcontains(var.ec2_config.operating_system, "RHEL") ? {
         # RHEL ```user-data``` CONFIGURATION
-        AWS_DEFAULT_REGION  = "${data.aws_region.current.name}"
-        AWS_ACCOUNT_ID      = "${data.aws_caller_identity.current.account_id}"
-        SYS_ARCH            = "amd64"
-        OS                  = "linux"
+        AWS_DEFAULT_REGION      = "${data.aws_region.current.name}"
+        AWS_ACCOUNT_ID          = "${data.aws_caller_identity.current.account_id}"
+        SYS_ARCH                = "amd64"
+        OS                      = "linux"
     } : {
         # WINDOWS ```user-data``` CONFIGURATION
-        AWS_DEFAULT_REGION  = "${data.aws_region.current.name}"
-        AWS_ACCOUNT_ID      = "${data.aws_caller_identity.current.account_id}"
+        AWS_DEFAULT_REGION      = "${data.aws_region.current.name}"
+        AWS_ACCOUNT_ID          = "${data.aws_caller_identity.current.account_id}"
     }
 }
