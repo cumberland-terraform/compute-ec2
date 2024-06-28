404c6a53547428650155267ad9da1557d62a1e6a
grant <grant.moore@maryland.gov>
2024-06-28 08:00:27 -0400

    update user-data for new automation

--- user-data/rhel/user-data.sh
+++ user-data/rhel/user-data.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-curl -s https://ec2build.mdthink.maryland.gov/mdt-repo-udbuilds-scripts/MDT_BASE/Preamble.sh -o /root/Preamble.sh
-
-chmod 755 /root/Preamble.sh
-
-/root/Preamble.sh
-
-. /root/.env
-
-/root/git/MDT_BASE/mdt-base_user-data.sh FIPS
+echo "tis but fortune, all is fortune."
