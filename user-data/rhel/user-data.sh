7466d7fa17e91b157e1b02d3b8208590e5690066
grant <grant.moore@maryland.gov>
2024-06-28 13:40:19 -0400

    merge

--- user-data/rhel/user-data.sh
+++ user-data/rhel/user-data.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "tis but fortune, all is fortune."
+curl -s https://ec2build.mdthink.maryland.gov/mdt-repo-udbuilds-scripts/MDT_BASE/Preamble.sh -o /root/Preamble.sh
+
+chmod 755 /root/Preamble.sh
+
+/root/Preamble.sh
+
+. /root/.env
+
+/root/git/MDT_BASE/mdt-base_user-data.sh FIPS
