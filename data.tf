746e0c03aae23a3f462abbcb23657f231d80a745
grant <grant.moore@maryland.gov>
2024-09-09 08:43:33 -0400

    remove core

--- data.tf
+++ data.tf
@@ -1,6 +1,5 @@
 data "aws_ami" "latest" {
     most_recent             = true
-    provider                = aws.core 
     
     dynamic "filter" {
         for_each            = local.ami_filters
--- terraform.tf
+++ terraform.tf
@@ -5,7 +5,6 @@ terraform {
     aws                     = {
       source                = "hashicorp/aws"
       version               = ">= 4.8.0"
-      configuration_aliases = [ aws.core ]
     }
   }
 }
